2e74ca5ad15fe25137c18007bea53a587999117d
tonykung <tonykung06@gmail.com>
2016-09-28 23:45:06 +0800

    worker queue

---
+++ WorkerQueueConsumer/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Examples
+{
+    public class Program
+    {
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private const string QueueName = "WorkerQueue_Queue";
+
+        public static void Main(string[] args)
+        {
+            Receive();
+            Console.ReadLine();
+        }
+
+        public static void Receive()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+            using (_connection = _factory.CreateConnection())
+            {
+                using (var channel = _connection.CreateModel())
+                {
+                    channel.QueueDeclare(QueueName, true, false, false, null);
+                    channel.BasicQos(0, 1, false);
+
+                    var consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume(QueueName, false, consumer);
+
+                    while (true)
+                    {
+                        var ea = consumer.Queue.Dequeue();
+                        var message = (Payment)ea.Body.DeSerialize(typeof(Payment));
+                        channel.BasicAck(ea.DeliveryTag, false);
+
+                        Console.WriteLine("---- Pyament Processed {0} : {1}", message.CardNumber, message.AmountToPay);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WorkerQueueProducer/Program.cs
@@ -0,0 +1,60 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Examples
+{
+    public class Program
+    {
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static IModel _model;
+        
+        private const string QueueName = "WorkerQueue_Queue";
+
+        static void Main(string[] args)
+        {
+            var payments = new List<Payment>();
+            for (var i = 0; i < 10; i++)
+            {
+                payments.Add(new Payment
+                {
+                    AmountToPay = 25.0m + i,
+                    CardNumber = "1234567890",
+                    Name = "my name " + i.ToString()
+                });
+            }
+
+            CreateConnection();
+
+            payments.ForEach((payment) =>
+            {
+                SendMessage(payment);
+            });
+
+            Console.ReadLine();
+        }
+
+        private static void SendMessage(Payment message)
+        {
+            _model.BasicPublish("", QueueName, null, message.Serialize());
+            Console.WriteLine(" Payment Sent {0}, ${1}", message.CardNumber, message.AmountToPay);
+        }
+
+        private static void CreateConnection()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+            _model.QueueDeclare(QueueName, true, false, false, null);
+        }
+    }
+}
