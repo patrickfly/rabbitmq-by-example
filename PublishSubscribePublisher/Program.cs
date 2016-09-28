3604dde49257366713a9933e313336bf7f8495b1
tonykung <tonykung06@gmail.com>
2016-09-29 00:15:12 +0800

    publish subscribe

---
+++ PublishSubscribePublisher/Program.cs
@@ -0,0 +1,60 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Examples
+{
+    public class Program
+    {
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static IModel _model;
+
+        private const string ExchangeName = "Publish_Subscribe_Exchange";
+
+        static void Main(string[] args)
+        {
+            var payments = new List<Payment>();
+            for (var i = 0; i < 10; i++)
+            {
+                payments.Add(new Payment
+                {
+                    AmountToPay = 25.0m + i,
+                    CardNumber = "1234567890",
+                    Name = "my name " + i.ToString()
+                });
+            }
+
+            CreateConnection();
+
+            payments.ForEach((payment) =>
+            {
+                SendMessage(payment);
+            });
+
+            Console.ReadLine();
+        }
+
+        private static void SendMessage(Payment message)
+        {
+            _model.BasicPublish(ExchangeName, "", null, message.Serialize());
+            Console.WriteLine(" Payment Sent {0}, ${1}", message.CardNumber, message.AmountToPay);
+        }
+
+        private static void CreateConnection()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+            _model.ExchangeDeclare(ExchangeName, "fanout", false);
+        }
+    }
+}
---
+++ PublishSubscribeSubscriber/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Examples
+{
+    public class Program
+    {
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static QueueingBasicConsumer _consumer;
+        private const string ExchangeName = "Publish_Subscribe_Exchange";
+
+        public static void Main(string[] args)
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+            using (_connection = _factory.CreateConnection())
+            {
+                using (var channel = _connection.CreateModel())
+                {
+                    var queueName = DeclareAndBindQueueToExchange(channel);
+                    channel.BasicConsume(queueName, true, _consumer);
+
+                    while (true)
+                    {
+                        var ea = _consumer.Queue.Dequeue();
+                        var message = (Payment)ea.Body.DeSerialize(typeof(Payment));
+
+                        Console.WriteLine("---- Pyament Processed {0} : {1}", message.CardNumber, message.AmountToPay);
+                    }
+                }
+            }
+        }
+
+        private static string DeclareAndBindQueueToExchange(IModel channel)
+        {
+            channel.ExchangeDeclare(ExchangeName, "fanout");
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queueName, ExchangeName, "");
+            _consumer = new QueueingBasicConsumer(channel);
+            return queueName;
+        }
+    }
+}
